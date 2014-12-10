251a82576ef68a677a9b1756dfad0f67f3aa4f2a
Philip Schill <philip.schill@gmx.de>
2014-12-10 11:24:52 +0100

    We can now deal with relative and absolute paths.

--- trainpredict.py
+++ trainpredict.py
@@ -23,12 +23,13 @@ class TrainPredictData(object):
     _tpd_test_gt_path = "test_gt_path"
     _tpd_test_gt_key = "test_gt_key"
 
-    def __init__(self, file_name, abs_paths=False):
+    def __init__(self, file_name, use_abs_paths=False):
         """
         Create a TrainPredictData instance by loading the given file.
         If the given file does not exist, it will be created.
 
         :param file_name: file name
+        :param use_abs_paths: when adding new data sets, the absolute path will be used instead of the relative path
         """
         # Check if the file can be opened with h5py.
         # If it does not exist, it will be created by h5py.
@@ -36,7 +37,35 @@ class TrainPredictData(object):
         f.close()
 
         self.file_name = file_name
-        self.abs_paths = abs_paths
+        self.use_abs_paths = use_abs_paths
+        self.base_path = os.path.relpath(os.path.dirname(self.file_name))
+
+    def _to_tpd_path(self, file_name):
+        """
+        Given a file name relative to the working directory, return the
+        file path relative to the .tpd file considering the abs path setting.
+
+        :param file_name: file name relative to working directory
+        :return: file name relative to .tpd file considering abs path setting
+        """
+        path = os.path.relpath(file_name, start=self.base_path)
+        if self.use_abs_paths:
+            path = os.path.abspath(path)
+        return path
+
+    def _to_rel_path(self, file_name):
+        """
+        Given a file name relative to the .tpd file, return the
+        file path relative to the current working directory.
+
+        :param file_name: file name relative to the .tpd file or abs path
+        :return: file name relative to the working directory
+        """
+        if os.path.isabs(file_name):
+            path = os.path.relpath(file_name)
+        else:
+            path = os.path.join(self.base_path, file_name)
+        return path
 
     def set_train_raw(self, file_name, h5_key):
         """Set file name and h5 key of the training raw data.
@@ -45,7 +74,7 @@ class TrainPredictData(object):
         :param h5_key: h5 key
         """
         with h5py.File(self.file_name, "a") as f:
-            f[self._tpd_train_raw_path] = file_name
+            f[self._tpd_train_raw_path] = self._to_tpd_path(file_name)
             f[self._tpd_train_raw_key] = h5_key
 
     def get_train_raw(self):
@@ -58,6 +87,7 @@ class TrainPredictData(object):
                 raise TPDError("get_train_raw(): The .tpd file does not contain raw training data.")
             file_name = f[self._tpd_train_raw_path].value
             h5_key = f[self._tpd_train_raw_key].value
+        file_name = self._to_rel_path(file_name)
         return file_name, h5_key
 
     def get_train_raw_data(self):
@@ -68,6 +98,37 @@ class TrainPredictData(object):
         file_name, h5_key = self.get_train_raw()
         return vigra.readHDF5(file_name, h5_key)
 
+    def set_train_gt(self, file_name, h5_key):
+        """Set file name and h5 key of the training gt data.
+
+        :param file_name: file name
+        :param h5_key: h5 key
+        """
+        with h5py.File(self.file_name, "a") as f:
+            f[self._tpd_train_gt_path] = self._to_tpd_path(file_name)
+            f[self._tpd_train_gt_key] = h5_key
+
+    def get_train_gt(self):
+        """Return file name and h5 key of the training raw data.
+
+        :return: file name, h5 key
+        """
+        with h5py.File(self.file_name, "r") as f:
+            if self._tpd_train_gt_path not in f.keys() or self._tpd_train_gt_key not in f.keys():
+                raise TPDError("get_train_gt(): The .tpd file does not contain gt training data.")
+            file_name = f[self._tpd_train_gt_path].value
+            h5_key = f[self._tpd_train_gt_key].value
+        file_name = self._to_rel_path(file_name)
+        return file_name, h5_key
+
+    def get_train_gt_data(self):
+        """Return the training gt data.
+
+        :return: training gt data
+        """
+        file_name, h5_key = self.get_train_gt()
+        return vigra.readHDF5(file_name, h5_key)
+
     def set_test_raw(self, file_name, h5_key):
         """Set file name and h5 key of the test raw data.
 
@@ -75,7 +136,7 @@ class TrainPredictData(object):
         :param h5_key: h5 key
         """
         with h5py.File(self.file_name, "a") as f:
-            f[self._tpd_test_raw_path] = file_name
+            f[self._tpd_test_raw_path] = self._to_tpd_path(file_name)
             f[self._tpd_test_raw_key] = h5_key
 
     def get_test_raw(self):
@@ -88,6 +149,7 @@ class TrainPredictData(object):
                 raise TPDError("get_test_raw(): The .tpd file does not contain raw test data.")
             file_name = f[self._tpd_test_raw_path].value
             h5_key = f[self._tpd_test_raw_key].value
+        file_name = self._to_rel_path(file_name)
         return file_name, h5_key
 
     def get_test_raw_data(self):
@@ -96,4 +158,35 @@ class TrainPredictData(object):
         :return: test raw data
         """
         file_name, h5_key = self.get_test_raw()
+        return vigra.readHDF5(file_name, h5_key)
+
+    def set_test_gt(self, file_name, h5_key):
+        """Set file name and h5 key of the test gt data.
+
+        :param file_name: file name
+        :param h5_key: h5 key
+        """
+        with h5py.File(self.file_name, "a") as f:
+            f[self._tpd_test_gt_path] = self._to_tpd_path(file_name)
+            f[self._tpd_test_gt_key] = h5_key
+
+    def get_test_gt(self):
+        """Return file name and h5 key of the test gt data.
+
+        :return: file name, h5 key
+        """
+        with h5py.File(self.file_name, "r") as f:
+            if self._tpd_test_gt_path not in f.keys() or self._tpd_test_gt_key not in f.keys():
+                raise TPDError("get_test_gt(): The .tpd file does not contain raw test data.")
+            file_name = f[self._tpd_test_gt_path].value
+            h5_key = f[self._tpd_test_gt_key].value
+        file_name = self._to_rel_path(file_name)
+        return file_name, h5_key
+
+    def get_test_gt_data(self):
+        """Return the test gt data.
+
+        :return: test gt data
+        """
+        file_name, h5_key = self.get_test_gt()
         return vigra.readHDF5(file_name, h5_key)
